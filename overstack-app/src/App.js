9f9cf108cf53ac9ee8f8f4c3e70482137da7f84c
Adarsh-nihal <adarsh474747@gmail.com>
2022-11-11 22:17:39 +0530

    added day-4

---
+++ overstack-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./Component/Navbar"
+import MainRoutes from './Component/MainRoutes';
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar />
+      <MainRoutes />
+ </div>
+  );
+}
+
+export default App;
