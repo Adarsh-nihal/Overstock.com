16aff21fae51a4d9bb69e04203ef986d4869b913
Adarsh-nihal <adarsh474747@gmail.com>
2022-11-13 15:03:40 +0530

    Added

--- overstack-app/src/Component/Utils/LocalStorage.js
+++ overstack-app/src/Component/Utils/LocalStorage.js
@@ -1,14 +1,5 @@
 // import {  useToast } from '@chakra-ui/react'
 
-function present(arr,value){
-    for(var i=0;i<arr.length;i++){
-        if(value.name===arr[i].name){
-            return true;
-        }
-        
-    }
-    return false;
-}
 
 
 function present(arr,value){
@@ -25,14 +16,12 @@ function present(arr,value){
 const saveData=(key,value)=>{
     
     let data=JSON.parse(localStorage.getItem(key))||[]
-//     console.log(present(data,value));
     if(!present(data,value)){
         localStorage.setItem(key,JSON.stringify([...data,value]))
      
     }
     
 
-    // localStorage.setItem(key,JSON.stringify([...data,value]))
     }
 
     const getData=(key)=>{
