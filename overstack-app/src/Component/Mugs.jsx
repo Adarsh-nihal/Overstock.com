2c6e865a4c5d4b51ca6c8da309e65d0490282d3f
Rati3010 <mratikanta1997@gmail.com>
2023-01-12 19:59:54 +0530

    pushed

--- overstack-app/src/Component/Mugs.jsx
+++ overstack-app/src/Component/Mugs.jsx
@@ -16,21 +16,21 @@ import { useSelector } from "react-redux";
 
 const Mugs = () => {
   const isAdmin = useSelector((state) => state.isAdmin);
-  const toast =useToast();
+  const toast = useToast();
 
   const handleDelete = (id) => {
     axios
       .delete(`https://stock-server.onrender.com/mugs/${id}`)
       .then((res) => {
-      setData(data.filter((e)=>{
-      return e.id!==id
-    }));
-    toast({
-      title: "Delete Successfull.",
-      status: "success",
-      duration: 3000,
-      isClosable: true,
-    });
+        setData(data.filter((e) => {
+          return e.id !== id
+        }));
+        toast({
+          title: "Delete Successfull.",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
       })
       .catch((err) => console.log(err));
   };
@@ -169,7 +169,7 @@ const Mugs = () => {
               </div>
               {isAdmin ? (
                 <div>
-                 <Button bg={"red"} width="60%" ml="20%" color="white" onClick={()=>handleDelete(item.id)} >Delete</Button>
+                  <Button bg={"red"} width="60%" ml="20%" color="white" onClick={() => handleDelete(item.id)} >Delete</Button>
                 </div>
               ) : null}
             </div>
