cf484c00445a8561acebef325ee23d2eb2faafd4
Rati3010 <mratikanta1997@gmail.com>
2023-01-12 20:02:11 +0530

    pushed

--- overstack-app/src/Component/ListContext.jsx
+++ overstack-app/src/Component/ListContext.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import MainList from './MainList'
 const ListCont = React.createContext()
 const ListContext = () => {
-    const [display,setDisplay] = useState();
-    const [edit2,setEdit2] = useState(true);
-    const handleSave = () =>{
-        if(display){
-          setEdit2(false);
-          handleDisplay(display);
-        }else{
-          alert('There is nothing in the display name');
-          return
-        }
+  const [display, setDisplay] = useState();
+  const [edit2, setEdit2] = useState(true);
+  const handleSave = () => {
+    if (display) {
+      setEdit2(false);
+      handleDisplay(display);
+    } else {
+      alert('There is nothing in the display name');
+      return
     }
-    const handleCancel = () =>{
-        setEdit2(false);
-    }
+  }
+  const handleCancel = () => {
+    setEdit2(false);
+  }
   return (
     <ListCont.Provider >
-     {props.children}
+      {props.children}
     </ListCont.Provider>
   )
 }
