41f1680f73f0043b9542e2dc296899c0920b55bf
Ashutosh Mahapatra <94985469+Ashutosh-041298-DEV@users.noreply.github.com>
2023-01-27 19:56:02 +0530

    Update App.js

--- overstack-app/src/App.js
+++ overstack-app/src/App.js
@@ -1,27 +1,21 @@
-
-import './App.css';
-import './App.css';
-import Navbar from "./Component/Navbar/Navbar"
-import MainRoutes from './Component/MainRoutes';
-import { Footer } from './Component/home/footer';
-import RespNav from "./Component/Navbar/respnav"
+import "./App.css";
+import Navbar from "./Component/Navbar/Navbar";
+import MainRoutes from "./Component/MainRoutes";
+import { Footer } from "./Component/home/footer";
+import RespNav from "./Component/Navbar/respnav";
 
 function App() {
- 
   return (
-
     <div className="App">
-    <div className="main-nav">   
-    <Navbar />
+      <div className="main-nav">
+        <Navbar />
+      </div>
+      <div className="resp-nav">
+        <RespNav />
+      </div>
+      <MainRoutes />
+      <Footer />
     </div>
-    <div className="resp-nav">
-     <RespNav/>
-    </div>
-    <MainRoutes />
-   
-    <Footer/>
-</div>
-
   );
 }
 
