d67421b53c989d79c13bca5814a66d4f272161c7
Ashutosh Mahapatra <94985469+Ashutosh-041298-DEV@users.noreply.github.com>
2023-01-27 19:57:56 +0530

    Update respnav.jsx

--- overstack-app/src/Component/Navbar/respnav.jsx
+++ overstack-app/src/Component/Navbar/respnav.jsx
@@ -1,17 +1,38 @@
 import React, { useState } from "react";
 import "./respnav.css";
 import { FaBars, FaShoppingCart } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutInitiate } from "../../Redux/AuthReducer/action";
 
 export default function RespNav() {
+  const defaultStyle = {
+    fontSize: "15px",
+    padding: "5px",
+  };
+  const activeStyle = {
+    fontSize: "15px",
+    padding: "5px",
+    fontWeight: "bold",
+    borderBottom: "2px solid red",
+  };
+  const adminStyle = {
+    fontSize: "15px",
+    fontWeight: "bold",
+    color: "red",
+  };
+  const adminActiveStyle = {
+    fontSize: "15px",
+    fontWeight: "bold",
+    color: "red",
+    borderBottom: "2px solid red",
+  };
   const [show, setShow] = useState(false);
-  const {isAdmin,isAuth}=useSelector((state)=>state)
-  const dispatch=useDispatch()
-  const handleSignout=()=>{
-    dispatch(logoutInitiate())
-  }
+  const { isAdmin, isAuth } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const handleSignout = () => {
+    dispatch(logoutInitiate());
+  };
 
   return (
     <div className="header">
@@ -21,15 +42,14 @@ export default function RespNav() {
             <FaBars />
           </button>
         </div>
-        <div
-          className="overstocklogo"
-         
-        >
-            <Link to="/"><img
-            className="overstockIndex"
-         src="https://i.postimg.cc/0yCKC2SV/svgexport-1.png"
-            alt=""
-          /></Link> 
+        <div className="overstocklogo">
+          <Link to="/">
+            <img
+              className="overstockIndex"
+              src="https://i.postimg.cc/0yCKC2SV/svgexport-1.png"
+              alt=""
+            />
+          </Link>
         </div>
         <div className="class-input">
           <div>
@@ -44,7 +64,7 @@ export default function RespNav() {
             {" "}
             <img src="https://i.postimg.cc/RCb3PDNz/User.png" alt="" />
           </Link>
-     
+
           <div className="chevronup">
             <img src="https://i.postimg.cc/DZHpvK2K/Chevron-Up.png" alt="" />
           </div>
@@ -54,24 +74,23 @@ export default function RespNav() {
             <div>My Reviews</div>
             <div>Gift Cards</div>
             <div>Help</div>
-            {
-               isAuth?(
-                <div onClick={handleSignout}>
-                  sign Out
-                </div>
-               ):(
-                <div>
-                 <Link to="/login"> Sign In </Link>
-                </div>
-               )
-              }
+            {isAuth ? (
+              <div onClick={handleSignout}>sign Out</div>
+            ) : (
+              <div>
+                <Link to="/login"> Sign In </Link>
+              </div>
+            )}
           </div>
         </div>
         <div className="class-account class-account-common">
           <div>
-          <Link to="/list">  <img src="https://i.postimg.cc/B6TrjykP/Heart.png" alt="" /></Link>
+            <Link to="/list">
+              {" "}
+              <img src="https://i.postimg.cc/B6TrjykP/Heart.png" alt="" />
+            </Link>
           </div>
-        
+
           <div className="chevronup">
             <img src="https://i.postimg.cc/DZHpvK2K/Chevron-Up.png" alt="" />
           </div>
@@ -85,16 +104,15 @@ export default function RespNav() {
             <div>Find a List</div>
           </div>
         </div>
-        <div
-          id="cart1"
-         
-          className="class-account"
-        >
-          <div className="Cart__Icon-Image" style={{fontSize:"25px"}}>
+        <div id="cart1" className="class-account">
+          <div className="Cart__Icon-Image" style={{ fontSize: "25px" }}>
             {/* <img src="https://i.postimg.cc/DyZNh8vX/Cart-Empty.png" alt="" />
             
             */}
-          <Link to="/Cart">  <FaShoppingCart/></Link>
+            <Link to="/Cart">
+              {" "}
+              <FaShoppingCart />
+            </Link>
           </div>
           {/* <p style={{ margin: "auto" }}>{0}</p> */}
         </div>
@@ -104,40 +122,68 @@ export default function RespNav() {
         className={`${show ? "show-div" : "hide-div"}`}
         onClick={() => setShow(!show)}
       >
-        <div>
-          <Link to="/mug">Mugs</Link>
-        </div>
-        <div>
-          <Link to="/cookingCutters">Cooking Cutters</Link>
-        </div>
-        <div>
-          <Link to="/fryer">Air Fryers</Link>
-        </div>
-        <div>
-          <Link to="/mixers">Mixers</Link>
-        </div>
-        <div>
-          <Link to="/lamps">Lamps</Link>
-        </div>
-        <div>
-          <Link to="/towels">Towels</Link>
-        </div>
-        <div>
-          <Link to="/blankets">Blankets</Link>
-        </div>
-        <div>
-          <Link to="/tree">Tree Skirts</Link>
-        </div>
-        <div>
-          <Link to="/garland">Garlands</Link>
-        </div>
-        {
-          isAdmin?(
-            <div style={{fontWeight:"bolder",color:"red"}}>
-              <Link to="/admin">Admin</Link>
-            </div>
-          ):null
-        }
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          to="/mugs"
+        >
+          Mugs
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          to="/cutters"
+        >
+          Cutters
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          to="/fryers"
+        >
+          Fryers
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          to="/mixers"
+        >
+          Mixers
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          to="/lamps"
+        >
+          Lamps
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          to="/towers"
+        >
+          Towers
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          to="/blankets"
+        >
+          Blankets
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          to="/trees"
+        >
+          Trees
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          to="/garlands"
+        >
+          Garlands
+        </NavLink>
+        {isAdmin ? (
+          <NavLink
+            style={({ isActive }) => (isActive ? adminActiveStyle : adminStyle)}
+            to="/admin"
+          >
+            Admin
+          </NavLink>
+        ) : null}
       </div>
     </div>
   );
